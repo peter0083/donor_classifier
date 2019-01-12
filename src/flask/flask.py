e040b01b4d8b981d8a297c653afe60a091d339d2
peter0083 <peter.tingyao@gmail.com>
2019-01-12 09:57:52 -0800

    add flask app

--- src/flask/flask.py
+++ src/flask/flask.py
@@ -1,2 +1,3 @@
 # ref: https://towardsdatascience.com/deploying-a-machine-learning-model-as-a-rest-api-4a03b865c166
-from flask 
+# ref2: https://towardsdatascience.com/a-flask-api-for-serving-scikit-learn-models-c8bcdaa41daa
+from flask import Flask
