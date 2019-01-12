89cd50fb6f4caf993fbe4a602bbdde714536ec56
peter0083 <peter.tingyao@gmail.com>
2019-01-12 11:02:43 -0800

    rename flask app to be consistent

--- src/flask/app.py
+++ src/flask/app.py
@@ -1,3 +1,8 @@
 # ref: https://towardsdatascience.com/deploying-a-machine-learning-model-as-a-rest-api-4a03b865c166
 # ref2: https://towardsdatascience.com/a-flask-api-for-serving-scikit-learn-models-c8bcdaa41daa
-from flask import Flask
+from flask import Flask, jsonify
+from sklearn.externals import joblib
+import pandas as pd
+
+app = Flask(__name__)
+
