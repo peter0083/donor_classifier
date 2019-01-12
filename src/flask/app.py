e2a8d1990da9e96b28d02a356e6769f58c4156ab
peter0083 <peter.tingyao@gmail.com>
2019-01-12 12:47:35 -0800

    testrun simple flask web app

--- src/flask/app.py
+++ src/flask/app.py
@@ -1,6 +1,7 @@
 # ref: https://towardsdatascience.com/deploying-a-machine-learning-model-as-a-rest-api-4a03b865c166
 # ref2: https://towardsdatascience.com/a-flask-api-for-serving-scikit-learn-models-c8bcdaa41daa
 # ref3: https://github.com/amirziai/sklearnflask
+# ref4: https://www.lynda.com/Flask-tutorials/Web-API-Development-Flask/
 import sys
 import os
 import shutil
@@ -13,3 +14,17 @@ import pandas as pd
 
 app = Flask(__name__)
 
+# load the model
+model = joblib.load('../modeling/rf_clf.pkl')
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    print("I have a model")
+    json = request.json
+    query = pd.get_dummies(pd.DataFrame(json_))
+
+    print(query.shape)
+
+if __name__ == "__main__":
+    app.run(debug=True)
