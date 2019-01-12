add91ea9ce3034858f10064ea00e9575ea9467fb
peter0083 <peter.tingyao@gmail.com>
2019-01-12 11:38:39 -0800

    saved random forest clf

--- src/flask/app.py
+++ src/flask/app.py
@@ -1,5 +1,12 @@
 # ref: https://towardsdatascience.com/deploying-a-machine-learning-model-as-a-rest-api-4a03b865c166
 # ref2: https://towardsdatascience.com/a-flask-api-for-serving-scikit-learn-models-c8bcdaa41daa
+# ref3: https://github.com/amirziai/sklearnflask
+import sys
+import os
+import shutil
+import time
+import traceback
+
 from flask import Flask, jsonify
 from sklearn.externals import joblib
 import pandas as pd
